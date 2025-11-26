27304ccfc1c5ed5f1362cd920e263f76055b1c5a
Caio18Godinho <caio.mota@sptech.school>
2025-11-25 22:48:21 -0300

    Finalização do site

--- Banco-de-Dados/Script - Banco de dados..sql
+++ Banco-de-Dados/Script - Banco de dados..sql
@@ -1,19 +1,21 @@
-create database FishWeb;
-use FishWeb;
+CREATE DATABASE FishWeb;
+USE FishWeb;
+CREATE TABLE Usuario (
+    idusuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(80),
+    senha VARCHAR(45),
+    CONSTRAINT uk_email UNIQUE (email) 
+);
 
-create table Usuario(
-idusuario int primary key auto_increment,
-nome varchar(45),
-email varchar(80),
-senha varchar(45));
-
-create table questionario (
-idquestionario int primary key auto_increment,
-vida_peixe int,
-fkUsuario int, 
-constraint chkusuario 
-foreign key (fkUsuario) 
-references Usuario(idusuario));
+CREATE TABLE questionario (
+    idquestionario INT AUTO_INCREMENT,
+    vida_peixe INT,
+    fkUsuario INT, 
+    primary key(idquestionario, fkUsuario),
+    CONSTRAINT fk_usuario_questionario FOREIGN KEY (fkUsuario)
+        REFERENCES Usuario (idusuario)
+);
 
 select * from Usuario;
 select * from Usuario join questionario on Usuario.idusuario = questionario.fkUsuario;
@@ -27,9 +29,11 @@ select count(fkUsuario) as partidas
         on Usuario.idusuario = questionario.fkUsuario group by fkUsuario
         having fkUsuario = 1;
         
-        select max(vida_peixe), min(vida_peixe), count(fkUsuario) as partidas
-        from questionario join Usuario 
-        on Usuario.idusuario = questionario.fkUsuario
+        select vida_peixe, idquestionario 
+        from questionario 
         where fkUsuario = 1;
         
         
+select max(vida_peixe) as MelhoresTentativas
+from questionario 
+where fkUsuario = 1;  
